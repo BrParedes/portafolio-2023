ccd95f804604d379105ae8103494d04ff0b5c061
Bryan <brparedess@gmail.com>
2023-02-28 05:49:32 -0300

    progress bar en conocimientos

--- src/app/conocimientos/conocimientos.component.ts
+++ src/app/conocimientos/conocimientos.component.ts
@@ -17,7 +17,7 @@ export class ConocimientosComponent implements OnInit {
   langButton : boolean = false;
   cursosButton : boolean = false;
   frameButton : boolean = false;
-  bdButton : boolean = false;
+  estiloButton : boolean = false;
   controlButton : boolean = false;
   phpCard : any;
 
@@ -29,9 +29,71 @@ export class ConocimientosComponent implements OnInit {
     this.langButton  = false;
     this.cursosButton  = false;
     this.frameButton  = false;
-    this.bdButton  = false;
+    this.estiloButton  = false;
     this.controlButton = false;
     
+  }
+
+
+  otrosBarProgress(){
+    const texto = document.querySelector('#otrosText1');
+    const barra = document.querySelector('#otrosBar1');
+
+    const texto2 = document.querySelector('#otrosText2');
+    const barra2 = document.querySelector('#otrosBar2');
+
+    
+
+    this.progressEffect( 10, texto, barra);
+    this.progressEffect( 52, texto2, barra2);
+    
+  }
+
+
+  cursosBarProgress(){
+    const texto = document.querySelector('#cursosText1');
+    const barra = document.querySelector('#cursosBar1');
+
+    const texto2 = document.querySelector('#cursosText2');
+    const barra2 = document.querySelector('#cursosBar2');
+
+    
+
+    this.progressEffect( 10, texto, barra);
+    this.progressEffect( 1, texto2, barra2);
+    
+  }
+
+
+
+  estiloBarProgress(){
+    const texto = document.querySelector('#estiloText1');
+    const barra = document.querySelector('#estiloBar1');
+
+    const texto2 = document.querySelector('#estiloText2');
+    const barra2 = document.querySelector('#estiloBar2');
+
+    const texto3 = document.querySelector('#estiloText3');
+    const barra3 = document.querySelector('#estiloBar3');
+
+    this.progressEffect( 80, texto, barra);
+    this.progressEffect( 80, texto2, barra2);
+    this.progressEffect( 80, texto3, barra3);
+  }
+
+  frameworkBarProgress(){
+    const texto = document.querySelector('#frameText1');
+    const barra = document.querySelector('#frameBar1');
+
+    const texto2 = document.querySelector('#frameText2');
+    const barra2 = document.querySelector('#frameBar2');
+
+    const texto3 = document.querySelector('#frameText3');
+    const barra3 = document.querySelector('#frameBar3');
+
+    this.progressEffect( 80, texto, barra);
+    this.progressEffect( 80, texto2, barra2);
+    this.progressEffect( 80, texto3, barra3);
   }
   LangBarProgress(){
     const texto = document.querySelector('#percentText1');
@@ -96,7 +158,7 @@ export class ConocimientosComponent implements OnInit {
         return this.frameButtonChange();
       }
       case 4: {
-        return this.bdButtonButtonChange();
+        return this.estiloButtonChange();
       }
       case 5: {
         return this.controlButtonChange();
@@ -108,47 +170,60 @@ export class ConocimientosComponent implements OnInit {
  
   langButtonChange(): void {
 
+    if(!this.langButton){
+      this.langButton = true;
+      this.cursosButton = false;
+      this.estiloButton = false;
+      this.frameButton = false;
+      this.controlButton = false;
+      this.LangBarProgress();
+    }
     
-    this.langButton = true;
-    this.cursosButton = false;
-    this.bdButton = false;
-    this.frameButton = false;
-    this.controlButton = false;
-    this.LangBarProgress();
   }
 
   cursosButtonChange(): void {
-    this.langButton = false;
-    this.cursosButton = true;
-    this.bdButton = false;
-    this.frameButton = false;
-    this.controlButton = false;
-
+    if(!this.cursosButton){
+      this.langButton = false;
+      this.cursosButton = true;
+      this.estiloButton = false;
+      this.frameButton = false;
+      this.controlButton = false;
+      this.cursosBarProgress();
+    }
   }
 
-  bdButtonButtonChange(): void {
-    this.langButton = false;
-    this.cursosButton = false;
-    this.bdButton = true;
-    this.frameButton = false;
-    this.controlButton = false;
-
+  estiloButtonChange(): void {
+    if(!this.estiloButton){
+      this.langButton = false;
+      this.cursosButton = false;
+      this.estiloButton = true;
+      this.frameButton = false;
+      this.controlButton = false;
+      this.estiloBarProgress();
+    }
   }
 
   frameButtonChange(): void {
-    this.langButton = false;
-    this.cursosButton = false;
-    this.bdButton = false;
-    this.frameButton = true;
-    this.controlButton = false;
+    if(!this.frameButton){
+      this.langButton = false;
+      this.cursosButton = false;
+      this.estiloButton = false;
+      this.frameButton = true;
+      this.controlButton = false;
+
+      this.frameworkBarProgress();
+    }
   }
 
   controlButtonChange(): void {
-    this.langButton = false;
-    this.cursosButton = false;
-    this.bdButton = false;
-    this.frameButton = false;
-    this.controlButton = true;
+    if(!this.controlButton){
+      this.langButton = false;
+      this.cursosButton = false;
+      this.estiloButton = false;
+      this.frameButton = false;
+      this.controlButton = true;
+      this.otrosBarProgress();
+    }
   }
 
   
